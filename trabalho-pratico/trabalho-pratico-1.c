89ab4e87739236f2950b50a13215437cbc43065f
Leonardo Castro <twelve.programming@gmail.com>
2025-05-20 17:53:35 -0400

    Arquivos C

---
+++ trabalho-pratico/trabalho-pratico-1.c
@@ -0,0 +1,8 @@
+/*Algoritmo para Construção de Árvore Binária a partir de Expressões Aritméticas
+- Analisar Expressão: Ler a expressão aritmética e identificar números, operadores e parênteses.
+- Construir Árvore: Organizar a árvore respeitando a precedência matemática.
+- Implementar Percurso: Garantir que a avaliação da expressão seja correta ao percorrer a árvore.
+O algoritmo deve reconhecer como a precedência de operações modifica a estrutura da árvore. Por exemplo:
+• A expressão 2 * 4 + 3resultará em uma estrutura arbórea que reflete a multiplicação sendo avaliada antes da adição.
+• A expressão 2 * (4 + 3)produzirá uma árvore onde a adição dentro dos parênteses é priorizada na estrutura.*/
+
